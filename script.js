f43a44cac9c4a0653453cdaa9288b74e035b53a5
lankok1 <antonpppmaster@mail.ru>
2024-07-17 21:20:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const btn = document.getElementById('btn')
 const container = document.getElementById('container')
-const audio = new Audio('/1000-7.mp3')
+const audio = new Audio('./1000-7.mp3')
 btn.addEventListener('click',() => {
     let start = 1000
     audio.play()
